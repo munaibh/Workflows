5fc351c2913b070cccc5913fa1f01923ed56f375
Munaib Hussain <munaibh@gmail.com>
2016-08-22 15:32:05 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp')
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('workflows are awesome');
+var coffeeSrc = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSrc)
+    .pipe(coffee({bare: true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
